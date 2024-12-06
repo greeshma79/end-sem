ab0714104c22f5f211ae1fb878d507efba20479b
greeshma79 <greeshmatummala79@gmail.com>
2024-12-06 19:01:56 +0530

    Intial commit

---
+++ EndSem/src/main/java/com/jfsd/EndSemApplication.java
@@ -0,0 +1,13 @@
+package com.jfsd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndSemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EndSemApplication.class, args);
+	}
+
+}
